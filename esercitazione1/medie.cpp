fb3749d4ade0de8259c168ee3715aa9e3a66ed46
balsamo-rebecca <s324542@studenti.polito.it>
2026-03-17 09:43:25 +0000

    Esercitazione 1

---
+++ esercitazione1/medie.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+	if (argc < 2) {
+		return 1;
+	}
+	
+	std::ifstream ifs(argv[1]);
+	if (!ifs.is_open()) {
+		return 1;
+	}
+	else {
+		std::string citta;
+		double t1, t2, t3, t4;
+	
+		while (ifs >> citta >> t1 >> t2 >> t3 >> t4) {
+			double media = (t1 + t2 + t3 + t4) / 4.0;
+			std::cout << citta << " " << media << "\n" ;
+		}
+	}
+	
+	return 0;
+	}
+	
+	
